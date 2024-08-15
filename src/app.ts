95f563810208a7251dccd55be0125eaf5ceda1a0
shadowfeind <travestroy007@gmail.com>
2024-08-15 08:26:25 +0545

    testing fastify

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv'
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import fjwt from '@fastify/jwt'
+import userRoutes from './modules/users.route'
+import { userSchmas } from './modules/users.schema'
+
+
+dotenv.config()
+
+export const fastify = Fastify({
+  logger: true
+})
+
+fastify.register(fjwt, {
+  secret: process.env.JWT_SECRET
+})
+
+fastify.decorate("/auth", async(req: FastifyRequest, reply:FastifyReply) => {
+  try {
+    await req.jwtVerify()
+  } catch (error) {
+    return reply.send(error)
+  }
+})
+
+fastify.get('/checkup', async () => {
+  return { status: 'OK' }
+})
+
+fastify.get('/ping', async () => {
+  return 'pong\n'
+})
+
+
+const start = async () => {
+  for(const schema of userSchmas){
+    fastify.addSchema(schema)
+  }
+    fastify.register(userRoutes, {prefix: "/api/v1/users"})
+  try {
+    await fastify.listen({ port: process.env.PORT })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
---
+++ src/modules/users.route.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from "fastify";
+import { getAllUsersHandler, loginHandler, registerUserHandler } from "./users.controller";
+import { $ref } from "./users.schema";
+
+async function userRoutes(server: FastifyInstance){
+    server.post("/",{
+        schema:{
+            body: $ref("createUserSchema"),
+            response:{
+                201: $ref("createUserResponseSchema")
+            }
+        }
+    }, registerUserHandler);
+
+    server.post("/login", {
+        schema: {
+            body: $ref("loginSchema"),
+            response: {
+                200: $ref('loginResponseSchema')
+            }
+        }
+    }, loginHandler)
+
+    server.get("/",{}, getAllUsersHandler)
+}
+
+export default userRoutes
